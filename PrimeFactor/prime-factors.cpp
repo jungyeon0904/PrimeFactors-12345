35ae482f3b245ea6faf16499d33cfea6e65c1ef4
Jungyeon Kim <wjddus0904@naver.com>
2025-05-02 14:14:02 +0900

    First commit

---
+++ PrimeFactor/prime-factors.cpp
@@ -0,0 +1,8 @@
+﻿#include <vector>
+
+class PrimeFactors {
+public:
+	vector<int> of(int i) {
+		return {};
+	}
+};
---
+++ PrimeFactor/prime-factors_test.cpp
@@ -0,0 +1,12 @@
+﻿#include <vector>
+#include "prime-factors.cpp"
+#include "gmock/gmock.h"
+
+using namespace std;
+
+TEST(PrimeFactors, Of1) {
+	PrimeFactors prime_factor;
+	vector<int> expected{};
+	EXPECT_EQ(expected, prime_factor.of(1));;
+
+}
